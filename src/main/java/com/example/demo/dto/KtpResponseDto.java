093d2c6810a23e2c182655f12edfc8621d1266c8
rnktlhr <rinakitluhur10@gmail.com>
2026-03-09 11:19:59 +0700

    membuat Ktp response dto pada project CRUD

---
+++ src/main/java/com/example/demo/dto/KtpResponseDto.java
@@ -0,0 +1,22 @@
+package com.example.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class KtpResponseDto {
+
+    private Integer id;
+    private String nomorKtp;
+    private String namaLengkap;
+    private String alamat;
+    private LocalDate tanggalLahir;
+    private String jenisKelamin;
+}
