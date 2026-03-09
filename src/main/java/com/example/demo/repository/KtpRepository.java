aa4eb40dc0d5256c14ebd9ee959baa8e8093e0c2
rnktlhr <rinakitluhur10@gmail.com>
2026-03-09 11:19:32 +0700

    membuat Ktp repository pada project CRUD

---
+++ src/main/java/com/example/demo/repository/KtpRepository.java
@@ -0,0 +1,14 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Ktp;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface KtpRepository extends JpaRepository<Ktp, Integer> {
+    Optional<Ktp> findByNomorKtp(String nomorKtp);
+
+    boolean existsByNomorKtp(String nomorKtp);
+}
